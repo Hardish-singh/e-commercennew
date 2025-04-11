65fa791915700c6e4563bb535dfbf38a5fd757d4
Hardish singh <hardishrajpu35@gmail.com>
2025-04-11 15:54:14 +0530

    size adjust

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,30 +14,32 @@ const Hero = () => {
       />
 
       {/* T-shirt image: visible only on desktop/tablet */}
-      <div className="absolute top-10 left-20 hidden md:block z-10">
+      <div className="absolute top-14 left-20 hidden md:block z-10">
         <Image
           src="/img1.png"
           alt="T-shirt"
           width={300}
-          height={400}
+          height={500}
           className="object-contain"
         />
       </div>
 
       {/* Mobile T-shirt image */}
-      <div className="absolute top-[70px] left-1/2 transform -translate-x-1/2 md:hidden z-10">
-        <Image
-          src="/img1.png"
-          alt="T-shirt"
-          width={220}
-          height={280}
-          className="object-contain "
-        />
-      </div>
+      <div className="absolute top-[50px] left-1/2 transform -translate-x-1/2 md:hidden z-10 w-[90vw] max-w-[620px]">
+  <Image
+    src="/img1.png"
+    alt="T-shirt"
+    width={300}
+    height={400}
+    className="object-contain"
+  />
+</div>
+
+
 
       {/* Text block */}
-      <div className="absolute z-20 px-3 w-full flex flex-col items-center md:items-start text-center md:text-left top-[55%] md:top-[330px] md:left-[6%]">
-        <h1 className="text-3xl md:text-5xl font-bold text-white drop-shadow">
+      <div className="absolute mt-3 z-20 px-3 w-full flex flex-col items-center md:items-start text-center md:text-left top-[60%] md:top-[340px] md:left-[6%]">
+        <h1 className="text-2xl md:text-5xl font-bold text-white drop-shadow">
           Luffy <span className="text-yellow-400 italic">Oversized</span>
         </h1>
         <div className="text-lg md:text-3xl font-bold text-white mt-1">T-shirts</div>
