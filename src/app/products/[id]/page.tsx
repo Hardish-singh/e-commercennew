34cdf34046c1a302e93da3e1e10a08c4fea47059
Hardish singh <hardishrajpu35@gmail.com>
2025-04-12 15:51:04 +0530

    block

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -103,10 +103,10 @@ const ProductPage = () => {
 
 
     <div className="space-y-4  ">
-      <button className=" hidden sm:block relative sm:left-0 left-45 sm:w-full w-[40%]  py-4 bg-white text-black rounded-full font-semibold hover:bg-opacity-90 transition-all">
+      <button className="  relative sm:left-0 left-45 sm:w-full w-[40%]  py-4 bg-white text-black rounded-full font-semibold hover:bg-opacity-90 transition-all">
         Add to Cart
       </button>
-      <button className="  py-4 border border-white/30 relative sm:left-0 left-45 sm:w-full w-[40%] rounded-full font-semibold hover:bg-white/10 transition-all">
+      <button className=" hidden sm:block  py-4 border border-white/30 relative sm:left-0 left-45 sm:w-full w-[40%] rounded-full font-semibold hover:bg-white/10 transition-all">
         Add to Wishlist
       </button>
     </div>
